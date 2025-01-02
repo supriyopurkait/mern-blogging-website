aad925406f3fe55df7d04bd5635c7e4413fd67f1
supriyopurkait <supriyopurkait7@gmail.com>
2025-01-02 23:24:45 +0530

    refactor: editor router

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { createContext, useState, useEffect } from "react"; // Fixed import
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
 import { lookInSession } from "./common/session";
+import Editor from "./pages/editor.pages";
 
 export const UserContext = createContext({});
 
@@ -19,6 +20,7 @@ const App = () => {
   return (
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <Routes>
+      <Route path="/editor" element={<Editor/>} />
         <Route path="/" element={<Navbar />}>
           <Route path="signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="signup" element={<UserAuthForm type="sign-up" />} />
