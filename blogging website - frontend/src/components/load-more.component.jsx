de8d41e0050a11a514d5a715fdc23db72cc10fea
supriyopurkait <supriyopurkait7@gmail.com>
2025-01-23 18:08:05 +0530

    feat: load_funcationability

---
+++ blogging website - frontend/src/components/load-more.component.jsx
@@ -0,0 +1,9 @@
+const LoadMoreDataBtn =({state,fetchDataFun}) =>{
+    if(state != null && state.totalDocs > state.results.length){
+        return(
+            <button onClick={()=>fetchDataFun({page: state.page+1})} className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"> load More</button>
+        )
+    }
+    
+}
+export default LoadMoreDataBtn;
