9730e2ebadcdb2019704f5b928df152b990a40e1
supriyopurkait <supriyopurkait7@gmail.com>
2025-01-23 23:58:15 +0530

    refactor: env url integration

--- blogging website - frontend/src/common/aws.jsx
+++ blogging website - frontend/src/common/aws.jsx
@@ -1,28 +1,25 @@
-
 const getImgURL = async (img) => {
-    try {
-      // Prepare the form data
-      const formData = new FormData();
-      formData.append('image', img);
-  
-      // Upload the image to the backend
-      const uploadResponse = await fetch('http://localhost:3000/get-upload-image', {
-        method: 'POST',
-        body: formData,
-      });
-  
-      if (uploadResponse.ok) {
-        const { id } = await uploadResponse.json();
-        console.log('Image uploaded successfully. ID:', id);
-        const imageUrl= `http://localhost:3000/image/${id}`;
-        return imageUrl;
-        // Retrieve the image from the backend using the returned ID
-        // const retrieveResponse = await fetch(`http://localhost:3000/image/${id}`);
-      } else {
-        console.error('Failed to upload image:', uploadResponse.statusText);
-      }
-    } catch (error) {
-      console.error('Error during image upload or retrieval:', error);
+  try {
+    // Prepare the form data
+    const formData = new FormData();
+    formData.append('image', img);
+
+    // Upload the image to the backend
+    const uploadResponse = await fetch(`${import.meta.env.VITE_SERVER_URL}/get-upload-image`, {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (uploadResponse.ok) {
+      const { id } = await uploadResponse.json();
+      console.log('Image uploaded successfully. ID:', id);
+      const imageUrl = `${import.meta.env.VITE_SERVER_URL}/${id}`;
+      return imageUrl;
+    } else {
+      console.error('Failed to upload image:', uploadResponse.statusText);
     }
-  };
+  } catch (error) {
+    console.error('Error during image upload or retrieval:', error);
+  }
+};
 export default getImgURL;
